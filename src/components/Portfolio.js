6cdba1b5c7887d40e3f43e85aba96e8996872d52
NganPham89 <algarvian2001@yahoo.com>
2023-06-15 17:47:21 -0400

    updated links

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,7 +4,10 @@ import "./Header.css"
 import ImgSoccer from "./assets/project-soccer.png";
 import ImgHomeHub from "./assets/project-homehub.png";
 import ImgBackend from "./assets/project-backend.jpg";
-import ImgWIP from "./assets/project-wip.jpg"
+import ImgBlog from "./assets/project-blog.jpg";
+import ImgWIP from "./assets/project-wip.jpg";
+import ImgCLI from "./assets/project-CLI.jpg";
+
 
 const projects = [
     {
@@ -26,6 +29,18 @@ const projects = [
         img: ImgBackend,
     },
     {
+        name: "Pokemon Blog",
+        github: "https://github.com/NganPham89/Pokemon-Blog",
+        url: "https://serene-savannah-81047.herokuapp.com/",
+        img: ImgBlog,
+    },
+    {
+        name: "Employee Management System",
+        github: "https://github.com/NganPham89/Employee-Info-Management",
+        url: "https://drive.google.com/file/d/1y7oAtQ_GyohJ_CHBhxgDY1fetrudCTNS/view",
+        img: ImgCLI,
+    },
+    {
         name: "Work in Progress",
         github: "Coming Soon",
         url: "Coming Soon",
@@ -37,15 +52,15 @@ const Portfolio = () => {
     return (
         <div className="post-body d-flex-row">
             <h2 className="font-os font-pink d-flex justify-content-center m-4">This is a collection of my most notable projects.</h2>
-            <div className="d-lg-flex justify-content-evenly">
+            <div className="d-lg-flex justify-content-around flex-wrap">
                 {projects.map((project) => {
                     return (
-                        <div className="d-flex-row mt-5 justify-content-center">
+                        <div className="d-flex-row my-5 mx-3 justify-content-center">
                             <a href={project.github} target="_blank" rel="noreferrer" className="nav-active d-flex justify-content-center">
                                 <h3 className="font-os">{project.name}</h3>
                             </a>
                             <a href={project.url} target="_blank" rel="noreferrer" className="d-flex justify-content-center">
-                                <img src={project.img} alt={project.name} width={400} className="border-blue"/>
+                                <img src={project.img} alt={project.name} width={450} height={300} className="border-blue"/>
                             </a>
                         </div>
                     )
