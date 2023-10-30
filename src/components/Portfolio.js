38f1e1cbe4522ddcc471242aba5ecd7c9fd850d9
NganPham89 <algarvian2001@yahoo.com>
2023-10-30 14:45:12 -0400

    added projects to portfolios

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,30 +5,48 @@ import ImgSoccer from "./assets/project-soccer.png";
 import ImgHomeHub from "./assets/project-homehub.png";
 import ImgBackend from "./assets/project-backend.jpg";
 import ImgBlog from "./assets/project-blog.jpg";
-// import ImgWIP from "./assets/project-wip.jpg";
 import ImgCLI from "./assets/project-CLI.jpg";
 import ImgJAAN from "./assets/project-event-manager.png";
+import ImgEcomFront from "./assets/project-ecommerce-frontend.jpg"
+import ImgEcomDash from "./assets/project-ecommerce-dashboard.jpg"
+
 
 
 const projectsList = [
     {
         id: 1,
-        name: "JAAN Event Manager",
+        name: "JAAN Event Manager (React)",
         github: "https://github.com/JaminHLO/jaan-event-manager",
         url: "https://sports-connect-e11a3bbe276b.herokuapp.com/",
         img: ImgJAAN,
-        description: "A club and event management site where user can host, find, or join sporting events in their areas."
+        description: "A club and event management site where user can host, find, or join sporting events in their areas. Built with React, MongoDB, and Tailwind"
     },
     {
         id: 2,
-        name: "HomeHub",
-        github: "https://github.com/Anabel-Espinoza/HomeHub",
-        url: "https://home-hub-management.herokuapp.com/",
-        img: ImgHomeHub,
-        description: "A home rental management system in which a homeowner has various tools to manage their properties and stay in touch with their tenants."
+        name: "E-Commerce Front End (Next.js 14)",
+        github: "https://github.com/NganPham89/rose-shop-frontend",
+        url: "https://rose-shop-frontend.vercel.app/",
+        img: ImgEcomFront,
+        description: "A store front to go along with an E-Commerce dashboard, built with Next.js 14, React, MySQL, and Tailwind."
     },
     {
         id: 3,
+        name: "E-Commerce Dashboard (Next.js 13)",
+        github: "https://github.com/NganPham89/rose-shop-admin",
+        url: "https://rose-shop-admin.vercel.app/",
+        img: ImgEcomDash,
+        description: "A CMS where shop owners can use to create and manage their store fronts. Built with Next.js 13, React, MySQL, and Tailwind."
+    },
+    {
+        id: 4,
+        name: "HomeHub (Handlebars)",
+        github: "https://github.com/Anabel-Espinoza/HomeHub",
+        url: "https://home-hub-management.herokuapp.com/",
+        img: ImgHomeHub,
+        description: "A home rental management system in which a homeowner has various tools to manage their properties and stay in touch with their tenants. Built with Handlebars, MySQL, and Bootstrap."
+    },
+    {
+        id: 5,
         name: "Soccer Betting Minigame",
         github: "https://github.com/NganPham89/Soccer-Betting-Mini-Game",
         url: "https://nganpham89.github.io/Soccer-Betting-Mini-Game/",
@@ -36,7 +54,7 @@ const projectsList = [
         description: "A fun simple game where the user can bet against a soccer betting site of an upcoming soccer match."
     },
     {
-        id: 4,
+        id: 6,
         name: "Social Media Backend",
         github: "https://github.com/NganPham89/Social-Media-Backend",
         url: "https://drive.google.com/file/d/1g9E9XEskw892D4jTHvWod0xJI46kedqt/view",
@@ -44,7 +62,7 @@ const projectsList = [
         description: "A functional simple backend for a social media site, including functionalities such as making posts, adding friends, sharing thoughts."
     },
     {
-        id: 5,
+        id: 7,
         name: "Pokemon Blog",
         github: "https://github.com/NganPham89/Pokemon-Blog",
         url: "https://serene-savannah-81047.herokuapp.com/",
@@ -52,7 +70,7 @@ const projectsList = [
         description: "A simple blog site where the user can create posts about pokemon-related contents and sharing their thoughts with people of similar interests."
     },
     {
-        id: 6,
+        id: 8,
         name: "Employee Management System",
         github: "https://github.com/NganPham89/Employee-Info-Management",
         url: "https://drive.google.com/file/d/1y7oAtQ_GyohJ_CHBhxgDY1fetrudCTNS/view",
